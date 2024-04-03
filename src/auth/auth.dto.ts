e36a879d5e08afce7d78b40d9438f5c1510c764f
정종문 <jeongjongmun@jeongjongmuns-MacBook-Air.local>
2024-04-03 17:49:35 +0900

    feat: swagger dto

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,23 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsString, Length } from 'class-validator'
 
 export namespace AuthRequestDto {
   export class SignIn {
+    @ApiProperty({ description: '이메일' })
     @IsEmail()
     email: string
 
+    @ApiProperty({ description: '비밀번호' })
     @IsString()
     @Length(10)
     password: string
   }
 
   export class SignUp {
+    @ApiProperty({ description: '이메일' })
     @IsEmail()
     email: string
 
+    @ApiProperty({ description: '비밀번호' })
     @IsString()
     @Length(10)
     password: string
 
+    @ApiProperty({ description: '닉네임' })
     @IsString()
     username: string
   }
@@ -25,6 +31,7 @@ export namespace AuthRequestDto {
 
 export namespace AuthResponseDto {
   export class Token {
+    @ApiProperty({ description: '토큰' })
     @IsString()
     access_token: string
 
